97fb7ec365a77f36d7ec12d311c2add1de575993
A3 <59868436+101br03k@users.noreply.github.com>
2020-07-24 11:55:20 +0200

    Update network-check.bat

--- network-check.bat
+++ network-check.bat
@@ -1,8 +1,17 @@
 DATE /T > ping2.txt
 TIME /T >> ping2.txt
 ping 8.8.8.8 > ping1.txt
-
-
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+more +1 "ping1.txt" > "ping1_.txt"
+ping1.txt >> ping2.txt
 
 #!/bin/sh
 clear
