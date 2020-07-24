ecd22d2f1dfd34a576ddb7998b401db473c636a9
A3 <59868436+101br03k@users.noreply.github.com>
2020-07-24 12:37:42 +0200

    Update network-check.bat

--- network-check.bat
+++ network-check.bat
@@ -1,13 +1,5 @@
 ping 8.8.8.8 > ping1.txt
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
-more +1 "ping1.txt" > "ping1_.txt"
+findstr /v /i /c:"pinging" /c:"reply" /c:"ping" myfile.txt /c:"packets" /c:"approximate" ping1.txt > ping1_.txt
 ping1.txt > ping2.txt
 
 
