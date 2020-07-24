0cca91ece4254a4038a0c3a52ce3232ada9ec643
A3 <59868436+101br03k@users.noreply.github.com>
2020-07-24 12:41:59 +0200

    Update network-check.bat

--- network-check.bat
+++ network-check.bat
@@ -1,5 +1,5 @@
 ping 8.8.8.8 > ping1.txt
-findstr /v /i /c:"pinging" /c:"reply" /c:"ping" myfile.txt /c:"Packets" /c:"Approximate" ping1.txt > ping1_.txt
+findstr /v /i /c:"pinging" /c:"reply" /c:"ping" myfile.txt /c:"Sent" /c:"round" /c:"Request" ping1.txt > ping1_.txt
 ping1.txt > ping2.txt
 
 
